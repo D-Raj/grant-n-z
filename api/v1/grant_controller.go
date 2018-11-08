803691af8bef1e7701cdc04a9659f59f969876c9
tomoyane <tohito0430@gmail.com>
2018-11-08 23:17:41 +0900

    impl group controller

---
+++ api/v1/grant_controller.go
@@ -0,0 +1,17 @@
+package v1
+//
+//import (
+//	"github.com/labstack/echo"
+//	"github.com/tomoyane/grant-n-z/di"
+//	"net/http"
+//)
+//
+//func GrantToken(c echo.Context) (err error) {
+//	token := c.Request().Header.Get("Authorization")
+//	errAuth := di.ProvideTokenService.VerifyToken(c, token, "")
+//	if errAuth != nil {
+//		return echo.NewHTTPError(errAuth.Code, errAuth)
+//	}
+//
+//	return c.JSON(http.StatusOK, map[string]bool {"authority": true})
+//}
