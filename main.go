6b1cbdec177d4c30c0bda1d6c8ac87d37267d2e1
tomoyane <tohito0430@gmail.com>
2018-11-11 20:58:29 +0900

    fix

--- main.go
+++ main.go
@@ -2,44 +2,23 @@ package main
 
 import (
 	"github.com/labstack/echo"
-	"github.com/tomoyane/grant-n-z/controller"
-	"github.com/tomoyane/grant-n-z/di"
-	"github.com/tomoyane/grant-n-z/domain"
-	"github.com/tomoyane/grant-n-z/domain/repository"
+	"github.com/tomoyane/grant-n-z/api"
+	"github.com/tomoyane/grant-n-z/api/v1"
 	"github.com/tomoyane/grant-n-z/infra"
-	"gopkg.in/go-playground/validator.v9"
 )
 
+func init() {
+	infra.Init()
+}
+
 func main() {
-	di.InitUserService(repository.UserRepositoryImpl{})
-	di.InitRoleService(repository.RoleRepositoryImpl{})
-	di.InitServiceService(repository.ServiceRepositoryImpl{})
-	di.InitMemberService(repository.MemberRepositoryImpl{})
-	di.InitTokenService(repository.TokenRepositoryImpl{}, repository.UserRepositoryImpl{})
-	di.InitPrincipalService(repository.PrincipalRepositoryImpl{}, repository.UserRepositoryImpl{},
-		repository.ServiceRepositoryImpl{}, repository.MemberRepositoryImpl{}, repository.RoleRepositoryImpl{})
-
-	infra.InitYaml()
-	infra.InitDB()
-	infra.MigrateDB()
-
 	e := echo.New()
-	e.Validator = &domain.GrantValidator{Validator: validator.New()}
+	e.HTTPErrorHandler = api.ErrorHandler
+	e.Validator = api.NewValidator()
 
-	e.POST("/v1/users", controller.PostUser)
-	e.PUT("/v1/users/:column", controller.PutUser)
+	e.POST("/v1/groups", v1.PostGroup)
+	e.POST("/v1/groups", v1.GetGroups)
+	e.POST("/v1/groups/:id", v1.GetGroup)
 
-	e.POST("/v1/roles", controller.PostRole)
-
-	e.POST("/v1/principals", controller.PostPrincipal)
-
-	e.POST("/v1/members", controller.PostMember)
-
-	e.GET("/v1/services", controller.GetService)
-	e.POST("/v1/services", controller.PostService)
-
-	e.POST("/v1/tokens", controller.PostToken)
-
-	e.POST("/v1/grants", controller.GrantToken)
 	e.Logger.Fatal(e.Start(":8080"))
 }
